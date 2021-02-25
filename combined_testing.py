0d883e17a647b5cf5de943f4fdb63eff2e5f10d1
יקי <60339558+Yakikim@users.noreply.github.com>
2021-02-25 16:49:22 +0200

    adding random and names

--- combined_testing.py
+++ combined_testing.py
@@ -3,10 +3,11 @@ The final testing of the program
 include backend + frontend testing
 """
 import backend_testing, frontend_testing
+import names, random
 
 if __name__ == '__main__':
-    user_id = 11 # input("Set user_id:")
-    user_name = 'Yanbkle' #input("set user_name:")
+    user_id = int(100000*(random.random())) # input("Set user_id:")
+    user_name = names.get_full_name() #input("set user_name:")
     #backend_testing.delete_all()
     backend_testing.post(user_id, user_name)
     if backend_testing.get(user_id) != user_name:
