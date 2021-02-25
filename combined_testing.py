f9bc09ea1ebff7610bc9ad0befa718e5b5a533b8
Yaki <yakikim@gmail.com>
2021-02-25 16:11:03 +0200

    Update for jenkins #2

--- combined_testing.py
+++ combined_testing.py
@@ -5,13 +5,13 @@ include backend + frontend testing
 import backend_testing, frontend_testing
 
 if __name__ == '__main__':
-    user_id = 10 # input("Set user_id:")
-    user_name = "JenkinsTest" #input("set user_name:")
+    user_id = 11 # input("Set user_id:")
+    user_name = 'Yanbkle' #input("set user_name:")
     #backend_testing.delete_all()
     backend_testing.post(user_id, user_name)
     if backend_testing.get(user_id) != user_name:
-        raise Exception("test failed: ")
+        raise Exception(f"test failed GET: {user_id}, {user_name}")
     if backend_testing.query(user_id) != user_name:
-        raise Exception("test failed")
+        raise Exception(f"test failed QUERY: {user_id}, {user_name}")
     if frontend_testing.selenium_test(user_id) != user_name:
-        raise Exception("test failed")
+        raise Exception(f"test failed SLENIUM_TEST: {user_id}, {user_name}")
