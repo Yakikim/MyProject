6057f5a45dcb71beb03ea1c1669659dbae3cfc3f
Yaki <yakikim@gmail.com>
2021-02-25 15:50:36 +0200

    Update for jenkins

--- combined_testing.py
+++ combined_testing.py
@@ -5,8 +5,8 @@ include backend + frontend testing
 import backend_testing, frontend_testing
 
 if __name__ == '__main__':
-    user_id = input("Set user_id:")
-    user_name = input("set user_name:")
+    user_id = 10 # input("Set user_id:")
+    user_name = "JenkinsTest" #input("set user_name:")
     #backend_testing.delete_all()
     backend_testing.post(user_id, user_name)
     if backend_testing.get(user_id) != user_name:
